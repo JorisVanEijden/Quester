18e2fb60fd9a0b07bbb84857fd48c9972ce7dbe8
JorisVanEijden <joris.vaneijden@gmail.com>
2019-08-26 19:28:22 +0200

    Gold item display fix

--- Quester/Item.cs
+++ Quester/Item.cs
@@ -15,33 +15,36 @@ namespace Quester
 
         public override string ToString()
         {
-            if (RewardType == RewardType.Gold || RewardType == RewardType.Unknown)
-            {
-                return (Category == ItemCategory.Random) ? "Random gold" : $"{ItemId} - {Category} gold";
-            }
-
             if (!ItemMapper.ItemMap.ContainsKey(Category))
             {
                 Console.Error.WriteLine("{0}: Failed to find item category {1} in item map", Program.Quest.Name, Category);
                 return "ERROR";
             }
 
-            string name;
-            try
+            string item = $"{Variable}: ";
+            if (RewardType == RewardType.Gold || RewardType == RewardType.Unknown)
             {
-                name = ItemId == 0xffff ? Category.ToString() : ItemMapper.ItemMap[Category][ItemId];
+                item += (Category == ItemCategory.Random) ? "Random gold" : $"{ItemId} - {Category} gold";
             }
-            catch (Exception )
+            else
             {
-                Console.Error.WriteLine($"{Program.Quest.Name}: no {ItemId} in {Category}");
-                throw;
+                try
+                {
+                    var name = ItemId == 0xffff ? Category.ToString() : ItemMapper.ItemMap[Category][ItemId];
+                    item += $"{RewardType} {Category} {name}";
+                }
+                catch (Exception )
+                {
+                    Console.Error.WriteLine($"{Program.Quest.Name}: no {ItemId} in {Category}");
+                    throw;
+                }
             }
 
-            var item = $"{Variable}: {RewardType} {Category} {name}";
             if (TextRecordId1 > 0)
                 item += $" [{TextRecordId1}]";
             if (TextRecordId2 > 0)
                 item += $" [{TextRecordId2}]";
+
             return item;
         }
     }
